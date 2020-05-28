ab615f86729cd6457bbf4986f2c94ce3d7dae2e4
jagadeesh <kesavajagadeesh123@gmail.com>
2020-05-28 16:48:33 +0530

    added all models for daashborad

--- dashboard/controlapp/urls.py
+++ dashboard/controlapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import home,products,customers
+from .views import home,products,customer
 
 urlpatterns = [
     path('',home,name="home"),
     path('products/',products,name="products"),
-    path('customer/',customers,name="customer"),
+    path('customer/<str:pk>/',customer,name="customer"),
 ]
--- dashboard/controlapp/views.py
+++ dashboard/controlapp/views.py
@@ -1,12 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import *
 # Create your views here.
 def home(request):
-	return render(request, "controlapp/dashboard.html")
+	orders=Order.objects.all()
+	customers=Customer.objects.all()
+
+	total_orders=orders.count()
+	pending=orders.filter(status='Pending').count()
+	deliverd=orders.filter(status='Deliverd').count()
+	context={
+		'orders':orders,
+		'customers':customers,
+		'total_orders':total_orders,
+		'pending':pending,
+		'deliverd':deliverd
+	}
+	return render(request, "controlapp/dashboard.html",context)
 
 def products(request):
-	return render(request, "controlapp/products.html")
 
-def customers(request):
-	return render(request, "controlapp/customers.html")
+	products=Product.objects.all()
+
+	context={
+		'products':products
+	}
+	return render(request, "controlapp/products.html",context)
+
+def customer(request,pk):
+	customer=Customer.objects.get(id=pk)
+
+	orders=customer.order_set.all()
+	total_orders=orders.count()
+	context={
+		'customer':customer,
+		'orders':orders,
+		'total_orders':total_orders
+	}
+	return render(request, "controlapp/customers.html",context)
